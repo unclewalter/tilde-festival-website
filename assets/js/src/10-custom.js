3134c3a95250f6d859a53151e638b2d04a5cbdc8
Kevan Atkins <kevan.atkins@gmail.com>
2016-10-07 17:22:13 +1100

    Forcing validation on submit for Calls forms. Oops.

--- assets/js/src/10-custom.js
+++ assets/js/src/10-custom.js
@@ -6,48 +6,50 @@ $(document).ready(function() {
     });
   });
   $("#submitEOI").click(function() {
-    var submissionDetails = $(".EOI-form").serializeObject();
-    $("#submitEOI").html("Submitting...");
-    submissionDetails.cv = {
-      type: "",
-      filename: ""
-    };
-    if (cvFile) {
-      var fname = $('#cvUpload').val().replace("C:\\fakepath\\", "");
+    $(".EOI-form").validate(function() {
+      var submissionDetails = $(".EOI-form").serializeObject();
+      $("#submitEOI").html("Submitting...");
       submissionDetails.cv = {
-        type: base64MimeType(cvFile),
-        fname: fname
+        type: "",
+        filename: ""
       };
-    }
-    $.ajax({
-      data: JSON.stringify(submissionDetails),
-      url: 'https://u7zjgs87ed.execute-api.eu-west-1.amazonaws.com/dev/eoi/submit',
-      type: 'POST',
-      processData: false,
-      crossDomain: true,
-      contentType: 'application/json; charset=UTF-8',
-      dataType: 'json',
-      success: function(data) {
-        if (data.errorMessage) {
-          alert("Error: " + data.errorMessage);
-          $("#submitEOI").html("Submit EOI");
-        } else {
-          if (data.cvfilename && document.getElementById('cvUpload')) {
-            var cvFile = document.getElementById('cvUpload').files[0]
-            uploadFile(cvFile, data.cvfilename, function() {
+      if (cvFile) {
+        var fname = $('#cvUpload').val().replace("C:\\fakepath\\", "");
+        submissionDetails.cv = {
+          type: base64MimeType(cvFile),
+          fname: fname
+        };
+      }
+      $.ajax({
+        data: JSON.stringify(submissionDetails),
+        url: 'https://u7zjgs87ed.execute-api.eu-west-1.amazonaws.com/dev/eoi/submit',
+        type: 'POST',
+        processData: false,
+        crossDomain: true,
+        contentType: 'application/json; charset=UTF-8',
+        dataType: 'json',
+        success: function(data) {
+          if (data.errorMessage) {
+            alert("Error: " + data.errorMessage);
+            $("#submitEOI").html("Submit EOI");
+          } else {
+            if (data.cvfilename && document.getElementById('cvUpload')) {
+              var cvFile = document.getElementById('cvUpload').files[0]
+              uploadFile(cvFile, data.cvfilename, function() {
+                $("#submitEOI").html("Submission successful!");
+                $("#submitEOI").attr("disabled", true);
+              });
+            } else {
               $("#submitEOI").html("Submission successful!");
               $("#submitEOI").attr("disabled", true);
-            });
-          } else {
-            $("#submitEOI").html("Submission successful!");
-            $("#submitEOI").attr("disabled", true);
+            }
           }
+          console.log(data);
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+          alert(thrownError);
         }
-        console.log(data);
-      },
-      error: function(xhr, ajaxOptions, thrownError) {
-        alert(thrownError);
-      }
+      });
     });
   });
 });
